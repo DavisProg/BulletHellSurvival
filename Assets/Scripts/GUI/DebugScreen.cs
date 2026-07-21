c66de22f43383c5e10a9dc8f331ad030a9ea13ca
DavisProg <davisplans06@gmail.com>
2026-07-21 13:22:26 +0300

    Debug menu added
    Debug menu added, only spell selection so far

---
+++ Assets/Scripts/GUI/DebugScreen.cs
@@ -0,0 +1,72 @@
+using TMPro;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using System;
+
+public class DebugScreen : MonoBehaviour
+{
+    private GameObject debugScreen;
+    private GameObject debugMenuScreen;
+    private GameObject spellSelectionDebugScreen;
+    private TMP_Dropdown spellSelectionDropdown;
+    private List<BaseSpell> spellList;
+    private List<TMP_Dropdown.OptionData> spellOptions = new List<TMP_Dropdown.OptionData>();
+
+    void Start()
+    {
+        debugScreen = transform.Find("Debug").gameObject;
+        debugMenuScreen = transform.Find("Debug/DebugMenu").gameObject;
+        spellSelectionDebugScreen = transform.Find("Debug/SpellSelectionDebugScreen").gameObject;
+        spellSelectionDropdown = transform.Find("Debug/SpellSelectionDebugScreen/SpellSelectionDropdown").GetComponent<TMP_Dropdown>();
+        spellList = GameObject.Find("Player").GetComponent<SpellSelection>().availableSpells;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.BackQuote))
+        {
+            Debug.Log("Debug button pressed");
+            initDebugScreen();
+        }
+    }
+    private void initDebugScreen()
+    {
+        if(debugScreen.activeSelf == false)
+        {
+            debugScreen.SetActive(true);
+            debugMenuScreen.SetActive(true);
+        }
+        else if (debugScreen.activeSelf == true)
+        {
+            debugScreen.SetActive(false);
+            debugMenuScreen.SetActive(false);
+            spellSelectionDebugScreen.SetActive(false);
+        }
+    }
+    public void initSpellSelectionDebugScreen()
+    {
+        Debug.Log("init called");
+        debugMenuScreen.SetActive(false);
+        spellSelectionDebugScreen.SetActive(true);
+
+        //Clear all options
+        spellOptions.Clear();
+        spellSelectionDropdown.ClearOptions();
+        //Add options
+        foreach (BaseSpell spell in spellList)
+        {
+            TMP_Dropdown.OptionData spellOption = new TMP_Dropdown.OptionData(spell.ToString(), null, new Color(1f, 0f, 0f));
+            spellOptions.Add(spellOption);
+            
+        }
+        spellSelectionDropdown.AddOptions(spellOptions);
+    }
+    public void selectDebugSpell()
+    {
+        spellList[spellSelectionDropdown.value].enable();
+    }
+
+  
+}
