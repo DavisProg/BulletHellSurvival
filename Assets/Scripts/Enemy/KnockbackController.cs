bf60d63ffd3365e5debc4267b93704fcf543585b
DavisProg <davisplans06@gmail.com>
2026-09-03 15:03:17 +0300

    Implemented knockback smoothing
    Enemies take knockback now ease in and out

--- Assets/Scripts/Enemy/KnockbackController.cs
+++ Assets/Scripts/Enemy/KnockbackController.cs
@@ -1,21 +1,34 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class KnockbackController : MonoBehaviour
 {
     Rigidbody2D rb;
     Enemy enemy;
+    [SerializeField] AnimationCurve curve;
     public void Awake()
     {
         enemy = gameObject.GetComponent<Enemy>();
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
-    public void takeKnockback(float strength, float delay, Vector2 direction)
+    public void takeKnockback(float intensity, float delay, Vector2 direction)
     {
         enemy.disableMovement();
         StopAllCoroutines();
-        rb.AddForce(direction * strength, ForceMode2D.Impulse);
-        StartCoroutine(Reset(delay));
+        StartCoroutine(Knockback(intensity, delay, direction));
+    }
+    IEnumerator Knockback(float intensity, float delay, Vector2 direction)
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < delay)
+        {
+            elapsedTime += Time.deltaTime;
+            float strength = curve.Evaluate(elapsedTime / delay);
+            rb.linearVelocity = direction * intensity * strength;
+            yield return null;
+        }
+        StartCoroutine(Freeze(0.6f));
     }
     private IEnumerator Reset(float delay)
     {
