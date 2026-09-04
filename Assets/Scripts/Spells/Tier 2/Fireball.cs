2b5f08ff35272c05f4f41c36cbe35ef7dca37c2d
DavisProg <davisplans06@gmail.com>
2026-09-04 12:22:17 +0300

    Simple Fireball placeholder
    Simple Fireball placeholder
    Organized spells into folders
    Started making a spellcrafting gui

---
+++ Assets/Scripts/Spells/Tier 2/Fireball.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Fireball : BaseSpell
+{
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] GameObject explosionPrefab;
+    [SerializeField] LayerMask enemyLayer;
+    [SerializeField] float damage;
+    [SerializeField] float projectileSize;
+    [SerializeField] float explosionSize;
+    [SerializeField] float speed;
+    [SerializeField] float duration;
+
+    void Awake()
+    {
+        targeting = new NearestTarget(enemyLayer);
+
+        defineCast();
+        setTrigger(new onLoop(this, cooldown));
+    }
+    protected void defineCast()
+    {
+        casting =  new CastProjectile(
+            projectilePrefab,
+            speed,
+            1,
+            projectileSize,
+            new IEffect[]
+            {
+                new RepeatEffect(new CastArea(explosionPrefab, explosionSize, duration, new DamageEffect(damage, this)), 0.1f, new RandomPoint(enemyLayer))
+            }
+        );
+    }
+}
